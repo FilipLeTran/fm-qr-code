019ddb0e65220080161d032892f558ff7eb14016
Filip Tran <filipletran@gmail.com>
2023-05-16 16:04:04 +0200

    add rounded corners for qr code picture

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <img src={qrCode} alt="qr-code" />
+        <img src={qrCode} alt="qr-code" id="qr-code"/>
       </div>
       <h1>Improve your front-end skills by building projects</h1>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
