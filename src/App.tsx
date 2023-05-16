1039f5f27e11a8141e2fef6d572da2c6f8da508b
Filip Tran <filipletran@gmail.com>
2023-05-16 15:57:32 +0200

    remove initial skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -14,17 +14,6 @@ function App() {
       </div>
       <h1>Improve your front-end skills by building projects</h1>
       <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
