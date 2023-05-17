7d8230814e389fe93e4a308e49bf96a725b85f99
Filip Tran <filipletran@gmail.com>
2023-05-17 18:59:11 +0200

    change deployment base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://filipletran.github.io/fm-qr-code/'
+  base: '/fm-qr-code/'
 })
